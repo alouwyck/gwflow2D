433fcc60dc362ea1474cfaa23db621ed0c131bc8
alouwyck <andy.louwyck@gmail.com>
2022-06-25 14:47:51 +0200

    documenting subpackage numeric

--- numeric/radial.py
+++ numeric/radial.py
@@ -1,5 +1,5 @@
 """
-Module containing specific classes to build finite-difference models for simulating 2D radial
+Module containing specific classes to build finite-difference models for simulating two-dimensional radial
 groundwater flow.
 """
 import numpy as np
